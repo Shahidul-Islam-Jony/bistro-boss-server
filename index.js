c0d07adf2cf1911108b9c75aa2fcb55e8b1d9f97
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-17 00:41:39 +0600

    M(68-7)  Create a JWT token and save it on local storage

--- index.js
+++ index.js
@@ -39,42 +39,61 @@ async function run() {
         const cartColletion = client.db('bistroDb').collection('carts');
 
 
-        // Users related api
+        // JWT related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            // res.cookie('token',{token})
+            res.send({ token })
+        })
 
-        app.get('/users',async(req,res)=>{
+
+        // middlewares for verify jwt token
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token',req.headers);
+            if(!req.headers.authorization){
+                return res.staus(401).send({message: 'forbidden access'})
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            
+            // next();
+        }
+
+        // Users related api
+        app.get('/users',verifyToken, async (req, res) => {
             const result = await userColletion.find().toArray()
             res.send(result);
         })
 
-        app.post('/users',async(req,res)=>{
+        app.post('/users', async (req, res) => {
             const user = req.body;
             // insert email if user does not exists
             // you can do this many way(1.Unique email, 2.upsert, 3.simple checking)
-            const query = {email:user.email}
+            const query = { email: user.email }
             const existingUser = await userColletion.findOne(query)
-            if(existingUser){
-                return res.send({message:'user already exists', insertedId:null})
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
             }
-            const result =await userColletion.insertOne(user);
+            const result = await userColletion.insertOne(user);
             res.send(result);
         })
 
-        app.patch('/users/admin/:id',async(req,res)=>{
-            const id =req.params.id;
-            const filter = {_id: new ObjectId(id)}
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
             const updatedDoc = {
-                $set:{
-                    role:'admin'
+                $set: {
+                    role: 'admin'
                 }
             }
-            const result = await userColletion.updateOne(filter,updatedDoc)
+            const result = await userColletion.updateOne(filter, updatedDoc)
             res.send(result)
         })
 
         //delete specific users
-        app.delete('/users/:id',async(req,res)=>{
+        app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await userColletion.deleteOne(query)
             res.send(result)
         })
@@ -82,37 +101,37 @@ async function run() {
 
         // menu related apis
         // find all menu data
-        app.get('/menu',async(req,res)=>{
+        app.get('/menu', async (req, res) => {
             const result = await menuColletion.find().toArray()
             res.send(result)
         })
         // Find all reviews data
-        app.get('/reviews',async(req,res)=>{
+        app.get('/reviews', async (req, res) => {
             const result = await reviewColletion.find().toArray()
             res.send(result)
         })
 
         // carts Collection
         // find carts data
-        app.get('/carts',async(req,res)=>{
+        app.get('/carts', async (req, res) => {
             const email = req.query.email;
             const query = {
                 email: email
             }
-            const result =await cartColletion.find(query).toArray();
+            const result = await cartColletion.find(query).toArray();
             res.send(result);
         })
 
         // add data to cart
-        app.post('/carts',async(req,res)=>{
+        app.post('/carts', async (req, res) => {
             const cartItem = req.body;
             const result = await cartColletion.insertOne(cartItem)
             res.send(result)
         })
         // delete a cart item
-        app.delete('/carts/:id',async(req,res)=>{
+        app.delete('/carts/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await cartColletion.deleteOne(query)
             res.send(result)
         })
