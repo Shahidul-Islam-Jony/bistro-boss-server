caa57bcb97d550d78833be9749a7da2d53c09a1d
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-16 23:38:30 +0600

    M(68-5)  Display All users and Create Make admin API

--- index.js
+++ index.js
@@ -58,9 +58,28 @@ async function run() {
             res.send(result);
         })
 
+        app.patch('/users/admin/:id',async(req,res)=>{
+            const id =req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updatedDoc = {
+                $set:{
+                    role:'admin'
+                }
+            }
+            const result = await userColletion.updateOne(filter,updatedDoc)
+            res.send(result)
+        })
+
+        //delete specific users
+        app.delete('/users/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await userColletion.deleteOne(query)
+            res.send(result)
+        })
 
 
-
+        // menu related apis
         // find all menu data
         app.get('/menu',async(req,res)=>{
             const result = await menuColletion.find().toArray()
